89fa886f2989d635a0f3187e2a949db855389fc1
Irwin Litvak <irwin.litvak@gmail.com>
2018-12-09 19:40:17 -0500

    removed unnecessary files, finished requirements for shortening of urls.

--- readline.js
+++ readline.js
@@ -8,23 +8,37 @@ let rl = readline.createInterface({
     output: process.stdout
 })
 
+// 
+let longUrls = [];
+let shortenedUrls = [];
+
 // When readline.js is run, askUserInput function will run.
 function askUserInput(){
-    rl.question('input URL to shorten: ', (userInput) => {
+    rl.question('Input url that you want shortened with http://<your url> ', (userInput) => {
         if(userInput === 'q' || userInput === 'Q'){
             rl.close();
         } else {
-            console.log(`URL entered: ${userInput}`);
+            // if we dont have the users URL inside our longURL's array
+            if(!longUrls.includes(userInput)){
+                longUrls.push(userInput)
 
-            // run axios post request with userInput
-            axios.post(`http://tinyurl.com/api-create.php?url=${userInput}`)
-            .then((response) => {
-                console.log('Response from TinyUrl: ', response.data);
-            })
-            .catch((error) => {
-                console.log('Error from TinyUrl: ', error);
-            })
+                // run axios post request with userInput
+                axios.post(`http://tinyurl.com/api-create.php?url=${userInput}`)
+                .then((response) => {
+                    console.log('Response from TinyUrl: ', response.data);
+                    shortenedUrls.push(response.data)
+                })
+                .catch((error) => {
+                    console.log('Error from TinyUrl: ', error);
+                })             
+            } else {
+                // if we have the URL that was entered in our shortened URL array
+                let foundURLIndex = longUrls.indexOf(userInput);
 
+                console.log(`URL already shortened: ${shortenedUrls[foundURLIndex]}`);
+            }
+
+            // prompt user again to input new URL to shorten
             askUserInput();
         }
     })
